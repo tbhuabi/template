d5072909dc39fa0435b1d3bb5b2b6703e96afdcc
18616392776 <464328895@qq.com>
2015-09-12 16:12:51 +0800

    html模板引擎

---
+++ template/js/data.js
@@ -0,0 +1,80 @@
+var data = {
+    type: '用户表',
+    userList: [{
+        name: '张三',
+        age: 23,
+        job: '司机',
+		sex: '男',
+		registerTime: 1441986770447,
+        relatives: {父亲: '隔壁老王',
+            母亲: '韦春花'
+        }
+    }, {
+        name: '李四',
+        age: 17,
+        job: '学生',
+		sex: '男',
+		registerTime: 1441986770447,
+        relatives: {姐姐: '李三',
+            弟弟: '李莲英',
+            表哥: '慕容复'
+        }
+    }, {
+        name: '王五',
+        age: 45,
+        job: '教师',
+		sex: '女',
+		registerTime: 1441986770447,
+        relatives: {姐姐: '王婆'
+        }
+    }, {
+        name: '赵六',
+        age: 26,
+        job: '码农',
+		sex: '女',
+		registerTime: 1441986770447,
+        relatives: {姐姐: '李三',
+            弟弟: '李莲英',
+            表哥: '慕容复'
+        }
+    }, {
+        name: '汪节',
+        age: 56,
+        job: '销售',
+		sex: '女',
+		registerTime: 1441986770447,
+        relatives: {姐姐: '李三',
+            弟弟: '李莲英',
+            表哥: '慕容复'
+        }
+    }, {
+        name: '刘邦',
+        age: 54,
+        job: '大王',
+		sex: '女',
+		registerTime: 1441986770447,
+        relatives: {姐姐: '李三',
+            弟弟: '李莲英',
+            表哥: '慕容复'
+        }
+    }, {
+        name: '韦小宝',
+        age: 14,
+        job: '太监',
+		registerTime: 1441986770447,
+        relatives: {姐姐: '李三',
+            弟弟: '李莲英',
+            表哥: '慕容复'
+        }
+    }, {
+        name: '张飞',
+        age: 26,
+        job: '将军',
+		sex: '女',
+		registerTime: 1441986770447,
+        relatives: {姐姐: '李三',
+            弟弟: '李莲英',
+            表哥: '慕容复'
+        }
+    }]
+};
